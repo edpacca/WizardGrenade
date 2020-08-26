1e609ac5c95904ed3f898852736e47afa0e2b019
Ed <59697268+Edpacca@users.noreply.github.com>
2020-08-26 23:10:45 +0100

    Round timer added and scoring

---
+++ WizardGrenade/Animation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardGrenade
+{
+    class Animation
+    {
+        //List<Texture2D> _frames;
+        //int horizontalTiles;
+        //int verticalTiles;
+
+        //public List<Texture2D> GenerateFrames(Texture2D spriteSheet, int frameWidth, int frameHeight)
+        //{
+        //    horizontalTiles = spriteSheet.Width / frameWidth;
+        //    verticalTiles = spriteSheet.Height / frameHeight;
+
+        //    for (int x = 0; x < horizontalTiles; x++)
+        //    {
+        //        for (int y = 0; y < verticalTiles; y++)
+        //        {
+
+        //        }
+        //    }
+
+        //}
+
+
+
+    }
+}
---
+++ WizardGrenade/Collision.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WizardGrenade
+{
+    class Collision
+    {
+        public static bool CollisionDetected(Sprite spriteA, Sprite spriteB)
+        {
+            Rectangle spriteARectangle = new Rectangle((int)spriteA.Position.X, (int)spriteA.Position.Y, spriteA.Size.Width, spriteA.Size.Height);
+            Rectangle spriteBRectangle = new Rectangle((int)spriteB.Position.X, (int)spriteB.Position.Y, spriteB.Size.Width, spriteB.Size.Height);
+            Rectangle intersectRectangle = Rectangle.Intersect(spriteARectangle, spriteBRectangle);
+
+            if (intersectRectangle.IsEmpty)
+            {
+                return false;
+            }
+
+
+            return true;
+        }
+    }
+}
